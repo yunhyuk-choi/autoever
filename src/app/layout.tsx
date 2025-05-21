83794a946dc2a84046b23c03ed3abb97578c294d
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-05-21 21:39:31 +0900

    FEAT: add footer v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppHeader from "@/components/common/AppHeader";
+import AppFooter from "@/components/common/AppFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AppHeader />
         {children}
+        <AppFooter />
       </body>
     </html>
   );
---
+++ src/components/common/AppFooter.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import { memo } from "react";
+
+function AppFooter() {
+  return (
+    <footer>
+      <div className="pr-12 pl-12 pb-11 pt-[34px] flex flex-col xl:flex-row-reverse align-middle justify-between space-y-6 mt-0 mb-0 mr-auto ml-auto bg-black text-gray-400 text-[14px] line-height-[24px]">
+        <div>
+          <span className="xl:flex justify-end">
+            <Link href={"https://privacy.kia.com/overview/full-policy"}>
+              <button
+                type="button"
+                className="pt-4 pb-4 hover:cursor-pointer hover:underline text-white mr-6"
+              >
+                <b className="text-[16px]">개인정보 처리방침</b>
+              </button>
+            </Link>
+            <button
+              type="button"
+              className="pt-4 pb-4 hover:cursor-pointer hover:underline text-white text-[16px]"
+            >
+              이용약관
+            </button>
+          </span>
+          <address className="xl:flex justify-end">
+            <span className="mr-3 ">
+              서울특별시 서초구 헌릉로 12 <em className="ml-3">기아㈜ </em>
+            </span>
+            <br className="md:hidden" />
+            <span className="mr-3">대표: 송호성, 최준영</span>
+            <br />
+            <span className="mr-3">사업자등록번호: 119-81-02316</span>
+            <br className="md:hidden" />
+            <span className="mr-3">통신판매번호: 2006-07935</span>
+            <br />
+            <span className="mr-3">
+              고객센터: <i>1833-4964</i>
+            </span>
+            <span className="mr-3 xl:mr-0">
+              제휴문의:
+              <Link href="mailto:kiabiz@kia.com" className="ml-1 underline">
+                kiabiz@kia.com
+              </Link>
+            </span>
+          </address>
+        </div>
+        <p>
+          <Image alt="KIA LOGO" width={80} height={48} src={"logo_kia.5deccfef5e3772b2d594.svg"} />©
+          2023 KIA CORP. All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default memo(AppFooter);
